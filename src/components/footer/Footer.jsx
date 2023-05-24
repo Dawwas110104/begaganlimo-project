c52e4a8f3266fc2a2fb87a67f05f635f8c07144d
Dawwas Inha Ulhaq <dawwas@MacBook-Pro-Dawwas.local>
2023-05-24 14:37:13 +0700

    Add Project

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="flex w-full flex-col items-center justify-between px-1 pb-8 pt-3 lg:px-8 xl:flex-row">
       <h5 className="mb-4 text-center text-sm font-medium text-gray-600 sm:!mb-0 md:text-lg">
         <p className="mb-4 text-center text-sm text-gray-600 sm:!mb-0 md:text-base">
-          ©{1900 + new Date().getYear()} <a rel="noopener" target="_blank" href="https://bit.ly/dawwas-portfolio">Dawwas.</a> All Rights Reserved.
+          ©{1900 + new Date().getYear()} <a href="https://bit.ly/dawwas-portfolio" target="_blank" rel="noopener">Dawwas.</a> All Rights Reserved.
         </p>
       </h5>
     </div>
